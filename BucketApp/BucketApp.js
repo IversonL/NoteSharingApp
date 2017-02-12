752cd943aa63f2391bb92b2d01767c8e75e59088
renellc <rcskca@gmail.com>
2017-02-11 21:32:46 -0600

    Dynamic Photo List
    -added Class Notes page to the sidebar
    -added dynamic photo list that updates list as soon as photo is added
    -added imgArray to MainController to store images
    -added an Options page and added it to the sidebar
    -general code cleanup and commented code for easier reading

--- BucketApp/BucketApp.js
+++ BucketApp/BucketApp.js
@@ -7,51 +7,63 @@ var app = angular.module('AngularUIBucketApp', [
 
 app.controller('MainController', ['$scope','$firebaseSimpleLogin','$location',
     function($scope,$firebaseSimpleLogin,$location) {
-    	counter = 0
-	$scope.pay = function() {
-    	$location.path('/userPay');       // redirect to home page after logout
-	}
 
+    // Changes path url
+  	$scope.pay = function() {
+      // redirect to home page after logout
+      $location.path('/userPay');
+  	}
     $scope.userHome = function() {
-    	$location.path('/userHome');    
-	}
-	$scope.signIn = function() {
-		$location.path('/signIn');
-	}
-	$scope.signUp = function() {
-		$location.path('/signUp');
-	}
-	$scope.classNotes = function() {
-		$location.path('/classNotes');
-	}
-	// Opens up the image page
-	// $scope.modal = function(){
-	// // Get the modal
-	// 	$scope.listObject = event.target.src;
-	// 	$scope.modal = document.getElementById('modal');
-	// // Displays the modal
-	// 	$scope.modal.style.display = "block";
-	// }
-	// // Closes the modal
-	// $scope.modalclose = function(){
-	// 	modal.style.display = "none";
-	// }
-	$scope.numfuck = function() {
-		counter += 1;
-		window.alert(counter);
-	}
-	$scope.modal = function(){
-		$scope.listObject = event.target.src;
-		modal = document.getElementById('modal');
-		modal.style.display = "block";
-	}
-	$scope.modalclose = function(){
-		modal.style.display = "none";
-	}
-}
-]);
+      // Redirects to home page
+      $location.path('/userHome');
+  	}
+  	$scope.signIn = function() {
+      // Redirects to sign-in page
+  		$location.path('/signIn');
+  	}
+  	$scope.signUp = function() {
+      // Redirects to sign-up page
+  		$location.path('/signUp');
+  	}
+  	$scope.classNotes = function() {
+      // Redirects to the class notes page
+  		$location.path('/classNotes');
+  	}
+    $scope.options = function() {
+      // Redirects to the options page
+  		$location.path('/options');
+  	}
+
+  	// Opens up the image page
+  	// $scope.modal = function(){
+  	// // Get the modal
+  	// 	$scope.listObject = event.target.src;
+  	// 	$scope.modal = document.getElementById('modal');
+  	// // Displays the modal
+  	// 	$scope.modal.style.display = "block";
+  	// }
+  	// // Closes the modal
+  	// $scope.modalclose = function(){
+  	// 	modal.style.display = "none";
+  	// }
+  	$scope.modal = function(){
+  		$scope.listObject = event.target.src;
+  		modal = document.getElementById('modal');
+  		modal.style.display = "block";
+  	}
+  	$scope.modalclose = function(){
+  		modal.style.display = "none";
+  	}
+
+    // Array that holds all the images uploaded
+    $scope.imgArray = [
+      'https://pbs.twimg.com/profile_images/770467680012890112/kSz1jtnn.jpg',
+      'http://vignette4.wikia.nocookie.net/finalfantasy/images/1/19/Kingdom_Hearts_Characters.jpg/revision/latest?cb=20130808121919'
+    ];
+}]);
 
 app.config(function($routeProvider, $locationProvider) {
+    // Routes to specific html page and loads it into app
     $routeProvider.when('/', {
         templateUrl: "signIn.html"
     });
@@ -72,5 +84,8 @@ app.config(function($routeProvider, $locationProvider) {
     });
     $routeProvider.when('/imgpage', {
 	templateUrl: "imgpage.html"
+    });
+    $routeProvider.when('/options', {
+	templateUrl: "options.html"
     });
 });
